19b52d6d9e76e6a9d8e8fb3ee8b5cbc8a4207675
= <=>
2026-06-13 14:35:53 +0200

    add }

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: ldeplace <ldeplace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 13:54:07 by ldeplace          #+#    #+#             */
-/*   Updated: 2026/06/13 14:09:34 by ldeplace         ###   ########.fr       */
+/*   Updated: 2026/06/13 14:35:14 by ldeplace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,4 @@ Zombie* newZombie(std::string name)
 {
     Zombie* zombieOnHeap = new Zombie(name);
     return zombieOnHeap;
+}
